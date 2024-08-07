6a54ae0b8f134041e09f6fd9c9f29498ed0c623c
Joaquin De Losada <montainproductions@gmail.com>
2024-08-06 22:36:04 -0500

    Work on Newscycle stories

--- JustPlayGameJam/Assets/Scripts/News/NewsCycle.cs
+++ JustPlayGameJam/Assets/Scripts/News/NewsCycle.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Company;
 
 public class NewsCycle : MonoBehaviour
 {
+    public static NewsCycle Instance { get; private set; }
+
     [SerializeField]
-    private GameObject[] newsList;
+    private List<string> newsTextList;
+
+    [SerializeField]
+    private List<string> baseNewsText, corigatedIronNewsText, matchesNewsText, portlandCementNewsText, electricMotorNewsText, steamHammerNewsText, leadAcidBatteryNewsText, lawnMowerNewsText, gasStoveNewsText;
 
     [SerializeField]
     private TextMeshProUGUI newsText;
 
-    private int currentNewsIndex;
+    private int randomStory;
 
     [SerializeField]
+    private float minTimePerStory, maxTimePerStory;
+
     private float timePerStory;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        currentNewsIndex = 0;
+        IncreaseNewsList();
+
         StartCoroutine(NewsFeed());
     }
 
@@ -31,19 +53,53 @@ public class NewsCycle : MonoBehaviour
 
     IEnumerator NewsFeed()
     {
+        randomStory = Random.Range(0, newsTextList.Count);
+        newsText.text = newsTextList[randomStory];
+
+        timePerStory = Random.Range(minTimePerStory, maxTimePerStory);
+
         yield return new WaitForSeconds(timePerStory);
-        newsList[currentNewsIndex].SetActive(false);
 
-        if(currentNewsIndex >= newsList.Length)
-        {
-            currentNewsIndex = 0;
-        }
-        else
-        {
-            currentNewsIndex++;
-        }
-
-        newsList[currentNewsIndex].SetActive(true);
         StartCoroutine(NewsFeed());
+    }
+
+    public void IncreaseNewsList(Company company = null)
+    {
+        if(company.businessName == CompanyName.none)
+        {
+            newsTextList.AddRange(baseNewsText);
+        }
+        else if (company.businessName == CompanyName.corrugatedGalvanisedIron)
+        {
+            newsTextList.AddRange(corigatedIronNewsText);
+        }
+        else if (company.businessName == CompanyName.matches)
+        {
+            newsTextList.AddRange(matchesNewsText);
+        }
+        else if (company.businessName == CompanyName.portlandCement)
+        {
+            newsTextList.AddRange(portlandCementNewsText);
+        }
+        else if (company.businessName == CompanyName.electricMotor)
+        {
+            newsTextList.AddRange(electricMotorNewsText);
+        }
+        else if (company.businessName == CompanyName.steamHammer)
+        {
+            newsTextList.AddRange(steamHammerNewsText);
+        }
+        else if (company.businessName == CompanyName.leadAcidBattery)
+        {
+            newsTextList.AddRange(leadAcidBatteryNewsText);
+        }
+        else if (company.businessName == CompanyName.lawnMower)
+        {
+            newsTextList.AddRange(lawnMowerNewsText);
+        }
+        else if (company.businessName == CompanyName.gasStoves)
+        {
+            newsTextList.AddRange(gasStoveNewsText);
+        }
     }
 }
